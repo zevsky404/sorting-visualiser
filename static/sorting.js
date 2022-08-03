908b5f5856bce1b4cb073a151fed83ea92224ecd
zevsky404 <chrissiguetter2001@gmail.com>
2022-08-03 21:14:54 +0200

    finish create bars function, remove last array when creating a new one

--- static/sorting.js
+++ static/sorting.js
@@ -1,16 +1,19 @@
 function createBars() {
   let heights = [];
+  const barDiv = document.getElementById('bars-div');
+  while (barDiv.lastElementChild) {
+    barDiv.removeChild(barDiv.lastElementChild);
+  }
 
   for (let i = 0; i <= 100; ++i) {
-    heights[i] = Math.floor(Math.random() * 101);
+    heights[i] = Math.ceil(Math.random() * 101);
   }
 
   for (let number in heights) {
     let newDiv = document.createElement('div');
     newDiv.className = "bar";
-    newDiv.style.height = Math.floor(heights[number] * 0.25) + "px";
+    newDiv.style.height = Math.floor(heights[number] * 5) + "px";
     newDiv.innerHTML = "";
-    const barDiv = document.getElementById('bars-div');
     barDiv.appendChild(newDiv);
   }
 }

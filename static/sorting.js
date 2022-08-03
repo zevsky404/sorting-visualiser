09484268ab3da50541fdb261a431648d0675c272
zevsky404 <chrissiguetter2001@gmail.com>
2022-08-03 20:43:17 +0200

    fix reload on createBars

--- static/sorting.js
+++ static/sorting.js
@@ -1,8 +1,17 @@
 function createBars() {
   let heights = [];
+
   for (let i = 0; i <= 100; ++i) {
     heights[i] = Math.floor(Math.random() * 101);
   }
 
+  for (let number in heights) {
+    let newDiv = document.createElement('div');
+    newDiv.className = "bar";
+    newDiv.style.height = Math.floor(heights[number] * 0.25) + "px";
+    newDiv.innerHTML = "";
+    const barDiv = document.getElementById('bars-div');
+    barDiv.appendChild(newDiv);
+  }
 }
 
